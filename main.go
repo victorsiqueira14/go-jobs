3b078c2afa8ede0825faf5d3b7521bd115f9548f
Victor <victorms.adm@gmail.com>
2023-08-01 17:44:22 -0300

    feat: Custom Logger

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main // declaração do pacote principal
 
-import "github.com/victorsiqueira14/go-jobs/router" // arquivo que contém a função main e inicializa tudo
+import (
+	"github.com/victorsiqueira14/go-jobs/config"
+	"github.com/victorsiqueira14/go-jobs/router"
+)
 
+var (
+	logger *config.Logger
+)
+
+// arquivo que contém a função main e inicializa tudo
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+
+	// initialize config
+	if err != nil {
+		logger.Errorf("Config initializing error: %v", err)
+		return
+	}
+
 	// initialize router
 	router.Initialize()
 }
